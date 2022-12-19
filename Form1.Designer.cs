77611fece020634e39a2c3fc64d4dd58a4f124ea
anoddyne <anoddyne@outlook.com>
2022-12-20 00:58:16 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace _3sem_2lr_kg
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_line = new System.Windows.Forms.Button();
+            this.button_arc = new System.Windows.Forms.Button();
+            this.button_ellipse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(713, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Очистка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_line
+            // 
+            this.button_line.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_line.Location = new System.Drawing.Point(12, 357);
+            this.button_line.Name = "button_line";
+            this.button_line.Size = new System.Drawing.Size(120, 23);
+            this.button_line.TabIndex = 4;
+            this.button_line.Text = "Прямая";
+            this.button_line.UseVisualStyleBackColor = true;
+            this.button_line.Click += new System.EventHandler(this.button_line_Click);
+            // 
+            // button_arc
+            // 
+            this.button_arc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_arc.Location = new System.Drawing.Point(12, 386);
+            this.button_arc.Name = "button_arc";
+            this.button_arc.Size = new System.Drawing.Size(120, 23);
+            this.button_arc.TabIndex = 5;
+            this.button_arc.Text = "Дуга окружности";
+            this.button_arc.UseVisualStyleBackColor = true;
+            this.button_arc.Click += new System.EventHandler(this.button_arc_Click);
+            // 
+            // button_ellipse
+            // 
+            this.button_ellipse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_ellipse.Location = new System.Drawing.Point(12, 415);
+            this.button_ellipse.Name = "button_ellipse";
+            this.button_ellipse.Size = new System.Drawing.Size(120, 23);
+            this.button_ellipse.TabIndex = 6;
+            this.button_ellipse.Text = "Эллипс";
+            this.button_ellipse.UseVisualStyleBackColor = true;
+            this.button_ellipse.Click += new System.EventHandler(this.button_ellipse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_ellipse);
+            this.Controls.Add(this.button_arc);
+            this.Controls.Add(this.button_line);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Button button1;
+        private Button button_line;
+        private Button button_arc;
+        private Button button_ellipse;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+using System.Windows.Forms.VisualStyles;
+
+namespace _3sem_2lr_kg
+{
+    public partial class Form1 : Form
+    {
+        Point p1, p2;
+        bool IsLine, IsArc, IsEllipse;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // нажатие на кнопку очистки
+        {
+            Graphics g = CreateGraphics();
+            g.FillRectangle(new SolidBrush(SystemColors.Control), ClientRectangle);
+            g.Dispose();
+        }
+
+        private void button_line_Click(object sender, EventArgs e) // нажатие на кнопку "Прямая"
+        {
+            IsLine = true;
+            IsArc = false;
+            IsEllipse = false;
+        }
+        private void button_arc_Click(object sender, EventArgs e) // нажатие на кнопку "Дуга окружности"
+        {
+            IsLine = false;
+            IsArc = true;
+            IsEllipse = false;
+        }
+        private void button_ellipse_Click(object sender, EventArgs e) // нажатие на кнопку "Эллипс"
+        {
+            IsLine = false;
+            IsArc = false;
+            IsEllipse = true;
+        }
+
+        private void Line(int I1, int J1, int I2, int J2)
+        {
+            Graphics g = CreateGraphics();
+            if (I1 > I2)
+            {
+                int i = I1; I1 = I2; I2 = i;
+                int j = J1; J1 = J2; J2 = j;
+            }
+            int dI = I2 - I1; int dJ = J2 - J1;
+            int S = 0;
+            if (dJ != 0)
+                S = dJ / Math.Abs(dJ);
+            int d = 2 * dJ - S * dI;
+            if (S * dJ <= dI)
+            {
+                int j = J1;
+                for (int i = I1; i <= I2; i++)
+                {
+                    g.FillRectangle(Brushes.Black, i, j, 2, 2);
+                    d += 2 * dJ;
+                    if (S * d >= 0)
+                    {
+                        j += S;
+                        d -= 2 * S * dI;
+                    }
+                }
+            }
+            else
+            {
+                int i = I1;
+                for (int j = J1; (J2 - j) * S >= 0; j += S)
+                {
+                    g.FillRectangle(Brushes.Black, i, j, 2, 2);
+                    d += 2 * S * dI;
+                    if (S * d >= 0)
+                    {
+                        d -= 2 * dJ;
+                        i++;
+                    }
+                }
+            }
+            g.Dispose();
+        }
+
+        private void Circle(int X1, int Y1, int r)
+        {
+            Graphics g = CreateGraphics();
+            int x = 0;
+            int y = r;
+            int delta = 2 - 2 * r;
+            int error = 0;
+            while (y >= 0)
+            {
+                // дуга 1-й четверти
+                g.FillRectangle(Brushes.Black, X1 + x, Y1 - y, 2, 2);
+                if (delta < 0)
+                {
+                    error = 2 * (delta + y) - 1;
+                    if (error <= 0)
+                        delta += 2 * ++x + 1;   // горизонтальный пиксель
+                    else
+                        delta += 2 * (++x - --y + 1);   // диагональный пиксель
+                }
+                else
+                {
+                    error = 2 * (delta - x) - 1;
+                    if (error > 0)
+                        delta -= 2 * --y + 1;   // вертикальный пиксель
+                    else
+                        delta += 2 * (++x - --y + 1);   // диагональный пиксель
+                }
+            }
+            g.Dispose();
+        }
+
+        private void Ellipse(int X1, int Y1, int rx, int ry)
+        {
+            Graphics g = CreateGraphics();
+            int x = 0;
+            int y = ry;
+            int rx2 = rx * rx;
+            int ry2 = ry * ry;
+            int delta = rx2 + ry2 - 2 * rx2 * ry;
+            int error = 0;
+            while (y >= 0)
+            {
+                // дуга 1-й четверти
+                g.FillRectangle(Brushes.Black, X1 + x, Y1 - y, 2, 2);
+                if (delta < 0)
+                {
+                    error = 2 * delta + rx2 * (2 * y - 1);
+                    if (error <= 0)
+                        delta += ry2 * (2 * ++x + 1);   // горизонтальный пиксель
+                    else
+                        delta += ry2 * (2 * ++x + 1) - rx2 * (2 * --y - 1);    // диагональный пиксель
+                }
+                else
+                {
+                    error = 2 * delta - ry2 * (2 * x + 1);
+                    if (error > 0)
+                        delta -= rx2 * (2 * --y + 1);   // вертикальный пиксель
+                    else
+                        delta += ry2 * (2 * ++x + 1) - rx2 * (2 * --y - 1);   // диагональный пиксель
+                }
+            }
+            g.Dispose();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            p1 = e.Location; //начальное положение курсора
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (IsLine)
+            {
+                p2 = e.Location; //конечное положение курсора
+                int r = (int)Math.Sqrt(Math.Pow((p2.X - p1.X), 2) + Math.Pow((p2.Y - p1.Y), 2));
+                Line(p1.X, p1.Y, p2.X, p2.Y);
+            }
+
+            if (IsArc)
+            {
+                p2 = e.Location;
+                int r = (int)Math.Sqrt(Math.Pow((p2.X - p1.X), 2) + Math.Pow((p2.Y - p1.Y), 2));
+                Circle(p1.X, p1.Y, r);
+            }
+
+            if (IsEllipse)
+            {
+                p2 = e.Location;
+                int r = (int)Math.Sqrt(Math.Pow((p2.X - p1.X), 2) + Math.Pow((p2.Y - p1.Y), 2));
+                Ellipse(p1.X, p1.Y, r, 2 * r);
+            }
+        }
+    }
+}
